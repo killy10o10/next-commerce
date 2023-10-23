a3794e733401d951abaa25acda18c8c4def6a873
killy10o10 <89058561+killy10o10@users.noreply.github.com>
2023-10-23 02:27:57 +0000

    Add navbar

---
+++ components/Navbar.tsx
@@ -0,0 +1,19 @@
+import {UserButton} from "@clerk/nextjs"
+import MainNav from "@/components/MainNav"
+import StoreSwitcher from "@/components/StoreSwitcher"
+
+const Navbar = () => {
+  return (
+    <nav className="border-b">
+      <div className="flex h-16 items-center px-4">
+        <StoreSwitcher />
+        <MainNav />
+        <div className="ml-auto flex items-center space-x-4">
+          <UserButton afterSignOutUrl="/" />
+        </div>
+      </div>
+    </nav>
+  )
+}
+
+export default Navbar
---
+++ components/StoreSwitcher.tsx
@@ -0,0 +1,9 @@
+const StoreSwitcher = () => {
+  return (
+    <div className="px-2">
+      store switcher
+    </div>
+  )
+}
+
+export default StoreSwitcher
